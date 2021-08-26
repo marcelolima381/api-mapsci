cac9d1d45a61120329140c1d17e15741bb151f52
Rander Gabriel <randergabriel98@gmail.com>
2021-08-26 19:28:32 -0300

    setup cors

--- searchfield/settings.py
+++ searchfield/settings.py
@@ -132,6 +132,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://localhost:3000/",
+    "http://localhost:3000/",
+    "https://vibrant-euclid-df9cfd.netlify.app/",
+]
 
 django_heroku.settings(locals())
