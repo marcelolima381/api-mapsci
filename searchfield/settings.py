9def9cf4101da19c109c6291aaf2081d5e8fb467
marcelolima-hotmart <marcelo.lima@hotmart.com>
2021-08-27 07:54:27 -0300

    cleanup

--- searchfield/settings.py
+++ searchfield/settings.py
@@ -135,6 +135,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "https://localhost:3000",
     "http://localhost:3000",
+    "https://127.0.0.1:3000",
+    "http://127.0.0.1:3000",
     "https://vibrant-euclid-df9cfd.netlify.app",
 ]
 
