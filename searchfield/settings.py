91f3c6a72db80343cee53b85309c0909b570af30
marcelolima-hotmart <marcelo.lima@hotmart.com>
2021-08-27 08:06:17 -0300

    allow all

--- searchfield/settings.py
+++ searchfield/settings.py
@@ -132,12 +132,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
-CORS_ALLOWED_ORIGINS = [
-    "https://localhost:3000",
-    "http://localhost:3000",
-    "https://127.0.0.1:3000",
-    "http://127.0.0.1:3000",
-    "https://vibrant-euclid-df9cfd.netlify.app",
-]
+
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://localhost:3000",
+#     "http://localhost:3000",
+#     "https://127.0.0.1:3000",
+#     "http://127.0.0.1:3000",
+#     "https://vibrant-euclid-df9cfd.netlify.app",
+# ]
 
 django_heroku.settings(locals())
